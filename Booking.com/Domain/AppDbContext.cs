a47df33199217e7ef5f0de6b1a661b19589ebf78
PudgePro99 <SemionTsi@yandex.ru>
2026-08-25 14:51:30 +0800

    Nastroil

---
+++ Booking.com/Domain/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Booking.com.Domain
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public DbSet<>
+    }
+}
---
+++ Booking.com/Domain/Enums/EnumRoomClass.cs
@@ -0,0 +1,9 @@
+﻿namespace Booking.com.Domain.Enums
+{
+    public enum RoomClass
+    {
+        Standart,
+        Superior,
+        Deluxe
+    }
+}
